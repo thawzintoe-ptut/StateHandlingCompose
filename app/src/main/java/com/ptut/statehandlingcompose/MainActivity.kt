784f4b1889f5a1c12a1cbd11c621a8e666dceac4
thawzintoe-codigo <thawzintoe@codigo.co>
2022-05-19 14:42:51 +0630

    viewmodel and hilt setup

--- app/src/main/java/com/ptut/statehandlingcompose/MainActivity.kt
+++ app/src/main/java/com/ptut/statehandlingcompose/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.ptut.statehandlingcompose
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.composethemeadapter.MdcTheme
 import com.ptut.statehandlingcompose.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ptut/statehandlingcompose/StateHandlingApp.kt
@@ -0,0 +1,7 @@
+package com.ptut.statehandlingcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StateHandlingApp : Application()
